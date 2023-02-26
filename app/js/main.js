3991b2194cf47330be08c6dc15a3233aaf2d68c0
Andrii Siakch <asekach@gmail.com>
2023-02-26 20:55:11 +0200

    save

---
+++ app/js/main.js
@@ -0,0 +1,131 @@
+$(function(){
+
+    // Scroll header
+	const $menu = $("header");
+    $(window).on('scroll', function(){    	
+    	$(this).scrollTop() > 20 ? $menu.addClass("scrolled-heder") : $menu.removeClass("scrolled-heder")
+    });
+
+    // Counts Spin
+    let show = true;
+    let countbox = ".benefits__number";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false;
+        let w_top = $(window).scrollTop(),
+            e_top = $(countbox).offset().top,
+            w_height = $(window).height(),
+            d_height = $(document).height(),
+            e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $(countbox)
+                .css('opacity', '1')
+                .spincrement({
+                    thousandSeparator: "",
+                    duration: 3000
+                });
+            show = false;
+        }
+    });
+
+    // Mobile menu
+    $('#toggle-mobile-menu').on('click', function(){
+        $(this).toggleClass('open-mobile')
+        $('.wrap__header--nav').slideToggle();
+    })
+
+    // Flipper
+    $('.card .button-flipp').on('click', 'a', function(e){
+        e.preventDefault();
+        $(this).parents('.item-card').toggleClass('is-flipped');
+    })
+
+    // OutTeam
+    $('#our-team-list-toggle').on('click', function(e){
+        e.preventDefault()
+        $(this).add('#our-team-list').toggleClass('isOpen');
+    })
+
+    // Grat slider
+    let sliderBig = $('.slider-grat')
+    if(sliderBig.length){
+        sliderBig.slick({
+            dots: false,
+            infinite: false,
+            speed: 200,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            autoplay: false,
+        });
+    }
+
+    // Partners slider
+    let sliderStories = $('.carusel-partners')
+    if(sliderStories.length){
+        sliderStories.slick({
+            dots: false,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 540,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+            ]
+        });
+    }
+
+    // Map
+    $('.element-toggle-info').on('click', function(){
+        $(this).siblings('.hidden-elenet')
+                .toggleClass('isOpen')
+                .parents()
+                .addClass('xIndex')
+                .siblings('.map-marker')
+                .removeClass('xIndex')
+                .find('.hidden-elenet')
+                .removeClass('isOpen')
+    })
+    $(document).on('click', function(e){
+        let $block = $(".map-marker")
+        if (!$block.is(e.target) && $block.has(e.target).length === 0){
+            $('.hidden-elenet').removeClass('isOpen')
+        }
+    });
+	
+    // Slider gallery
+    // let listPrevGallery = $('.list-prev-gallery .item'),
+    //     sliderGallery = $('.slider-gallery');
+    // if(sliderGallery.length){
+    //     sliderGallery.slick({
+    //         dots: false,
+    //         infinite: false,
+    //         speed: 500,
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1,
+    //         fade: true,
+    //     });
+    //     sliderGallery.on('afterChange', (event, slick, currentSlide, nextSlide) => {
+    //         listPrevGallery.removeClass('active').eq(currentSlide).addClass('active')
+    //     });
+    //     listPrevGallery.on('click', function(){
+    //         sliderGallery.slick('slickGoTo', listPrevGallery.index(this));
+    //     })
+    // }
+    
+});
